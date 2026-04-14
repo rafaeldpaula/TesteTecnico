faf3567f4da5a2d1954a9dba4690826e7db2013e
Rafael de Paula <rafael@onetouchhealth.net>
2026-04-14 14:50:23 -0300

    chore: refactoring the sctructure to accommodate the EF Core

---
+++ Repository/EFTaskRepository.cs
@@ -0,0 +1,22 @@
+﻿using TesteTecnico.Entities;
+
+namespace TesteTecnico.Repository
+{
+    public class EFTaskRepository : ITaskRepository
+    {
+        public void Add(TaskItem task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IReadOnlyList<TaskItem> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskItem? GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
